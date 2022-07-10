239d8b2b133d2b2871a1dae28b1ce0c7fb18c9ae
Michael Prather <mikeprather20@gmail.com>
2022-07-10 16:45:50 -0500

    swapping

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,29 +1,42 @@
 class ParkingGarage():
 
     def __init__(self):
-        self.tickets=[0,1,2,3,4,5,6,7,8,9,10]
-        self.parkingSpaces=[0,1,2,3,4,5,6,7,8,9,10]
-        self.currentTicket={}
+        self.tickets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.parkingSpaces = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.currentTicket = {}
 
     def takeTicket(self, tickets, parkingSpaces):
         for i in range(len(self.tickets and self.parkingSpaces)):
             self.tickets[i] = self.tickets[i] - 1
             print(self.tickets)
             if i == 0:
-                print ('Sorry, the Parking Garage is full!')
+                print('Sorry, the Parking Garage is full!')
         return
-        
+
     def payForParking(self, currentTicket):
-        payment = input('Please pay your ticket')
-        pass
+        amount = input('Please pay your 1 dollar ticket fee.')
+        if amount == 1:
+            self.currentTicket['Paid'] = True
+        else: amount == 0:
+            self.currentTicket['Unpaid'] = False
+                
 
     def leaveGarage(self, tickets, parkingSpaces, currentTicket):
-        pass
+        for i in range(len(self.tickets and self.parkingSpaces)):
+            self.parkingSpaces[i] = self.parkingSpaces[i] + 1
+
+            if self.currentTicket == True:
+                print('Thank you, have a nice day!')
+            else:
+                self.currentTicket == False
+                amount = input('Please pay your 1 dollar ticket fee.')
+
 
 def main():
     car = ParkingGarage
 
     while True:
         pass
+
 
 main()
